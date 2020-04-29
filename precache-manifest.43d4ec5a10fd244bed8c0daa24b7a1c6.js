9febf1d17a369cce29a1378d146ae8e6286daad1
DevRayHe <DevRayHe@gmail.com>
2020-04-29 22:23:55 +1000

    Updates

---
+++ precache-manifest.43d4ec5a10fd244bed8c0daa24b7a1c6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b3313e76e34682f104473c82fdd7361",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "3d2db00c1e951dd89bf6",
+    "url": "/robofriends/static/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "9d64c91a5db3a674036f",
+    "url": "/robofriends/static/css/main.1544c800.chunk.css"
+  },
+  {
+    "revision": "3d2db00c1e951dd89bf6",
+    "url": "/robofriends/static/js/2.3f95b59c.chunk.js"
+  },
+  {
+    "revision": "6287c65a6963df837d60dc49bb220910",
+    "url": "/robofriends/static/js/2.3f95b59c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d64c91a5db3a674036f",
+    "url": "/robofriends/static/js/main.faed7fc0.chunk.js"
+  },
+  {
+    "revision": "8adbf24e3312ffc2ae3e",
+    "url": "/robofriends/static/js/runtime-main.403370f6.js"
+  },
+  {
+    "revision": "e41c06be219cac241cec57bf658b87a1",
+    "url": "/robofriends/static/media/Spacestation-K7BVX.e41c06be.woff"
+  }
+]);
